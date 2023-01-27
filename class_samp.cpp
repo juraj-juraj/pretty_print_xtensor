ad439290900a91346b694d8448d110d199cdeb2a
juraj Novosad <juraj.novosad@phonexia.com>
2023-01-27 14:16:06 +0100

    refactor and print xtensor

--- class_samp.cpp
+++ class_samp.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <xtensor/xarray.hpp>
+#include <xtensor/xview.hpp>
 
 class Person{
 public:
@@ -19,11 +20,20 @@ int main(){
     Person p1("Person1");
     int a = 0;
     xt::xarray<double> arr1
-      {{1.0, 2.0, 3.0},
-       {2.0, 5.0, 7.0},
-       {2.0, 5.0, 7.0}};
+        {{1.0, 2.0, 3.0},
+         {2.0, 5.0, 7.0},
+         {2.0, 5.0, 7.0}};
     auto arr2 = arr1 + 5;
     auto arr3 = arr2 /2;
+    auto xview3 = xt::view(arr3, 1, xt::all());
+    auto arr3_eval = xt::eval(arr3);
+    auto arr3_t = xt::transpose(arr3);
+
+    xt::xtensor<double, 2> xtens1
+        {{1.0, 2.0, 3.0},
+         {2.0, 5.0, 7.0},
+         {2.0, 5.0, 7.0}};
+
     
     std::cout << "Person len: " << p1.getLen() << std::endl;
 }
--- printer.py
+++ printer.py
@@ -1,8 +1,43 @@
 import gdb
 import numpy as np
 
+class printerInterface:
+    """Interface for pretty printer implementation
+    
+    Defines prototype for children and to_string methods.
+    """
+    def __init__(self, value:gdb.Value):
+        """Saving value to member called mValue"""
+        self.m_value = value
+     
+    def children(self) -> tuple:
+        """Implement iterator hich gives out data."""
+        pass
+    
+    def to_string(self) -> str:
+        """Implement method to cast whole object to string, printable to the output.
+        This method is sort of complementary to children method"""
+        pass
+    
+def c_to_py_type(c_name: str):
+    if(c_name in ['float', 'double', 'long double']):
+        return float
+    elif(c_name.startswith('std::complex')):
+        return complex
+    else:
+        return int
+    
+def fetch_array(begin_it: gdb.Value, end_it:gdb.Value) -> np.array:
+    n_size = end_it - begin_it
+    d_type = c_to_py_type(str(begin_it.dereference().type)) #get datatype of one value as is in C++
+    
+    return np.fromiter(map(lambda index: int((begin_it+index).dereference()), range(n_size)), dtype=d_type)
 
-class prettyXarray:
+def shape_from_object(in_val : gdb.Value) -> np.array:
+    in_val = str(in_val).strip('{}').replace(' ', '').split(',')
+    return np.array([int(i) for i in in_val]) 
+
+class prettyXarray (printerInterface):
     """Xtensor pretty printer.
 
        prikazy:
@@ -16,30 +51,43 @@ class prettyXarray:
     """
 
     def __init__(self, value: gdb.Value):
-        self.value = value
+        super().__init__(value)
 
     def children(self):
-        shape = prettyXarray.fetch_array(self.value['m_shape']['m_begin'], self.value['m_shape']['m_end'])
-        data = prettyXarray.fetch_array(self.value['m_storage']['p_begin'], self.value['m_storage']['p_end'])
+        shape = fetch_array(self.m_value['m_shape']['m_begin'], self.m_value['m_shape']['m_end'])
+        data = fetch_array(self.m_value['m_storage']['p_begin'], self.m_value['m_storage']['p_end'])
         data = data.reshape(shape)
         yield('shape', str(shape))
         yield('data', str(data))
 
     # def to_string(self):
-    #     shape = prettyXtensor.fetch_array(self.value['m_shape']['m_begin'], self.value['m_shape']['m_end'])
-    #     data = prettyXtensor.fetch_array(self.value['m_storage']['p_begin'], self.value['m_storage']['p_end'])
+    #     shape = fetch_array(self.m_value['m_shape']['m_begin'], self.m_value['m_shape']['m_end'])
+    #     data = fetch_array(self.m_value['m_storage']['p_begin'], self.m_value['m_storage']['p_end'])
     #     data = data.reshape(shape)
-    #     return f"shape is {shape}, data: {data}"
+    #     return f"shape is {shape}, data: {data}"    
 
-    @staticmethod
-    def fetch_array(begin_it: gdb.Value, end_it:gdb.Value, mDtype=int) -> np.array:
-        n_size = end_it - begin_it
-        return np.fromiter(map(lambda index: int((begin_it+index).dereference()), range(n_size)), dtype=mDtype)        
+class prettyXtensor (printerInterface):
+    """Xtensor pretty
+        Prints data, if available, and shape.     
+    """
+    def __init__(self, value):
+        super().__init__(value)    
+    
+    def children(self):
+        shape = shape_from_object(self.m_value['m_shape']['_M_elems'])
+        data = fetch_array(self.m_value['m_storage']['p_begin'], self.m_value['m_storage']['p_end'])
+        data = data.reshape(shape)
+        yield('shape', str(shape))
+        yield('data', str(data))
 
 
 def xtensor_resolve(val):
-    if(str(val.type) == 'xt::xarray'):
-        return prettyXarray(val)
-
-
+    match str(val.type):
+        case 'xt::xarray':
+            return prettyXarray(val)
+        case 'xt::xtensor':
+            return prettyXtensor(val)
+        case _:
+            return
+        
 gdb.pretty_printers.append(xtensor_resolve)
