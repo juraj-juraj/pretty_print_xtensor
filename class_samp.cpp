d16c2d0696a52aad09ca937ced0aa0e43aa2a87f
juraj Novosad <juraj.novosad@phonexia.com>
2023-01-31 12:53:58 +0100

    Format sources and add setup guide

--- class_samp.cpp
+++ class_samp.cpp
@@ -1,64 +1,49 @@
 #include <iostream>
 #include <string>
 #include <xtensor/xarray.hpp>
-#include <xtensor/xview.hpp>
 #include <xtensor/xfixed.hpp>
 #include <xtensor/xlayout.hpp>
-#include <string>
+#include <xtensor/xview.hpp>
 
-class Person{
+class Person {
 public:
-    Person(std::string name) : mName(std::move(name)) { }
+  Person(std::string name) : mName(std::move(name)) {}
 
-    size_t getLen()
-    {
-        return mName.length();
-    }
+  size_t getLen() { return mName.length(); }
 
 private:
-    std::string mName;
+  std::string mName;
 };
 
-int main(){
-    Person p1("Person1");
-    xt::xarray<int> a2 = {};
-    xt::xarray<double> arr1
-      {{1.0, 2.0, 3.0},
-       {2.0, 5.0, 7.0},
-       {2.0, 5.0, 7.0}};
-    auto xview1 = xt::view(arr1, 1, xt::all());
-    auto xrow1 = xt::row(arr1, 0);
-    auto trans1 = xt::transpose(arr1);
-    auto arr2 = arr1 + 5;
-    auto arr3 = arr2 /2;
-    auto xview3 = xt::view(arr3, 1, xt::all());
-    auto arr3_eval = xt::eval(arr3);
-    auto arr3_t = xt::transpose(arr3);
+int main() {
+  Person p1("Person1");
+  xt::xarray<int> a2 = {};
+  xt::xarray<double> arr1{{1.0, 2.0, 3.0}, {2.0, 5.0, 7.0}, {2.0, 5.0, 7.0}};
+  auto xview1 = xt::view(arr1, 1, xt::all());
+  auto xrow1 = xt::row(arr1, 0);
+  auto trans1 = xt::transpose(arr1);
+  auto arr2 = arr1 + 5;
+  auto arr3 = arr2 / 2;
+  auto xview3 = xt::view(arr3, 1, xt::all());
+  auto arr3_eval = xt::eval(arr3);
+  auto arr3_t = xt::transpose(arr3);
 
-    xt::xtensor<double, 2> xtens1
-        {{1.0, 2.0, 3.0},
-         {2.0, 5.0, 7.0},
-         {2.0, 5.0, 7.0}};
+  xt::xtensor<double, 2> xtens1{
+      {1.0, 2.0, 3.0}, {2.0, 5.0, 7.0}, {2.0, 5.0, 7.0}};
 
+  xt::xtensor<double, 2> xtens2;
 
-    xt::xtensor_fixed<double, xt::xshape<3, 2>> xtensfix1
-        {{1.0, 2.0},
-         {2.0, 5.0},
-         {2.0, 5.0}};
-    
-    xt::xtensor_fixed<double, xt::xshape<3, 2, 4>> xtensfix2 {};
-    xt::xarray<int> a3 = {{{1, 2, 3, 4}, {6, 7, 8, 9}}};
+  xt::xtensor_fixed<double, xt::xshape<3, 2>> xtensfix1{
+      {1.0, 2.0}, {2.0, 5.0}, {2.0, 5.0}};
 
-    xt::xarray<double, xt::layout_type::row_major> ar
-        {{1.0, 2.0, 3.0},
-         {2.0, 5.0, 7.0},
-         {2.0, 5.0, 7.0}};
-    xt::xarray<double, xt::layout_type::dynamic> ac
-        {{1.0, 2.0, 3.0},
-         {2.0, 5.0, 7.0},
-         {2.0, 5.0, 7.0}};
+  xt::xtensor_fixed<double, xt::xshape<3, 2, 4>> xtensfix2{};
+  xt::xarray<int> a3 = {{{1, 2, 3, 4}, {6, 7, 8, 9}}};
 
+  xt::xarray<double, xt::layout_type::row_major> ar{
+      {1.0, 2.0, 3.0}, {2.0, 5.0, 7.0}, {2.0, 5.0, 7.0}};
+  xt::xarray<double, xt::layout_type::column_major> ac{
+      {1.0, 2.5, 3.0}, {2.0, 5.0, 7.0}, {2.0, 5.0, 7.0}};
 
-    std::string str1 = "Hello";
-    std::cout << "Person len: " << p1.getLen() << std::endl;
+  std::string str1 = "Hello";
+  std::cout << "Person len: " << p1.getLen() << std::endl;
 }
--- xtensor_printer.py
+++ xtensor_printer.py
@@ -1,56 +1,67 @@
 import gdb
 import numpy as np
 
+
 class printerInterface:
     """Interface for pretty printer implementation
-    
+
     Defines prototype for children and to_string methods.
     """
-    def __init__(self, value:gdb.Value):
+
+    def __init__(self, value: gdb.Value):
         """Saving value to member called mValue"""
         self.value = value
-     
+
     def children(self) -> tuple:
         """Implement iterator hich gives out data."""
         pass
-    
+
     def to_string(self) -> str:
         """Implement method to cast whole object to string, printable to the output.
         This method is sort of complementary to children method"""
         pass
-    
+
+
 def c_to_py_type(c_name: str):
     match(c_name):
-        case gdb.TYPE_CODE_FLT| gdb.TYPE_CODE_FLT:
+        case gdb.TYPE_CODE_FLT | gdb.TYPE_CODE_FLT:
             return float
         case gdb.TYPE_CODE_COMPLEX:
             return complex
         case gdb.TYPE_CODE_INT:
             return int
-    
-def fetch_array(begin_it: gdb.Value, end_it:gdb.Value) -> np.array:
+
+
+def fetch_array(begin_it: gdb.Value, end_it: gdb.Value) -> np.array:
     n_size = end_it - begin_it
-    d_type = c_to_py_type(begin_it.dereference().type.code) #get datatype of one value as is in C++
-    
+    # get datatype of one value as is in C++
+    d_type = c_to_py_type(begin_it.dereference().type.code)
+
     return np.fromiter(map(lambda index: d_type((begin_it+index).dereference()), range(n_size)), dtype=d_type)
 
-def shape_from_object(in_val : gdb.Value) -> np.array:
+
+def shape_from_object(in_val: gdb.Value) -> np.array:
     in_val = str(in_val).strip('{}').replace(' ', '').split(',')
-    return np.array([int(i) for i in in_val]) 
+    return np.array([int(i) for i in in_val])
+
 
 class prettyXarray (printerInterface):
     def __init__(self, value: gdb.Value):
         super().__init__(value)
         if(str(self.value.type) == 'xt::xarray'):
-            self.shape = fetch_array(self.value['m_shape']['m_begin'], self.value['m_shape']['m_end'])
+            self.shape = fetch_array(
+                self.value['m_shape']['m_begin'], self.value['m_shape']['m_end'])
         elif(str(self.value.type) == 'xt::xtensor'):
             self.shape = shape_from_object(self.value['m_shape']['_M_elems'])
-        
+
     def children(self):
-        if(self.shape.size == 0):
+        if(np.sum(self.shape, dtype=int) == 0):
             return
-        order = "F" if str(self.value['m_layout']) == "xt::layout_type::column_major" else "C" # F like fortran style, for collumn major
-        indices = np.nditer(np.zeros(tuple(self.shape)), flags=['multi_index'], order=order)
+        # F like fortran style, for collumn major
+        order = "F" if str(
+            self.value['m_layout']) == "xt::layout_type::column_major" else "C"
+        indices = np.nditer(np.zeros(tuple(self.shape)),
+                            flags=['multi_index'], order=order)
         it = self.value['m_storage']['p_begin']
         while(it != self.value['m_storage']['p_end']):
             yield(f"{indices.multi_index}", it.dereference())
@@ -58,10 +69,11 @@ class prettyXarray (printerInterface):
             indices.iternext()
 
     def to_string(self):
-        return f"{self.value.type} with shape {self.shape}"   
+        return f"{self.value.type} with shape {self.shape}"
 
     def display_hint(self):
         return 'string'
+
 
 def xtensor_resolve(val):
     match str(val.type):
@@ -69,5 +81,6 @@ def xtensor_resolve(val):
             return prettyXarray(val)
         case _:
             return
-        
+
+
 gdb.pretty_printers.append(xtensor_resolve)
